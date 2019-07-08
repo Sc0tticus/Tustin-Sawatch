f9ddf57023c29a69fb90c4c4cd8692dbc8a7c523
drbarq <J.Tustin@gmail.com>
2019-07-08 10:52:51 -0600

    cleaned up functions

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const api = "https://api.sawatchlabs.com/models/13/2017"
+const apiUrl = "https://api.sawatchlabs.com/models/13/2017"
 const attributes = ["vehicle_year", "make", "vehicle_model", "displacement", "cylinders", "class"]
 const swtTable  =  document.getElementById("swt-table")
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetchApiInformation(api)
-        .then(sawatchData => sortSawatchData(sawatchData))
-        .then((sortedSawatchData) => addDataToTable(sortedSawatchData))
-  });
+    fetchCarInformation(apiUrl)
+        .then(carData => sortCarData(carData))
+        .then(sortedCarData => addDataToTable(sortedCarData))
+});
 
-fetchApiInformation = api => {
-    return fetch(api)
+const fetchCarInformation = apiUrl => {
+    return fetch(apiUrl)
         .then(response => response.json())
         .then(result => [result.data][0])
         .catch(error => console.error(error))
 }
 
-sortSawatchData = sawatchData => {
-    sortedSawatchData = sawatchData.sort((a, b) => {
-        if(a.vehicle_model > b.vehicle_model) {
+const sortCarData = carData => {
+    return carData.sort((a, b) => {
+        if (a.vehicle_model > b.vehicle_model) {
             return 1;
         } else if (a.vehicle_model < b.vehicle_model) {
             return -1;
@@ -25,24 +25,23 @@ sortSawatchData = sawatchData => {
             return 0
         }
     })
-    return sortedSawatchData
 }
 
-addDataToTable = sortedSawatchData => {
-    sortedSawatchData.map((carInfo) => {
+const addDataToTable = sortedCarData => {
+    sortedCarData.map(carInfo => {
         createRow(carInfo, attributes)  
     })
 }
 
-createRow = (carInfo, attributes) => {
+const createRow = (carInfo, attributes) => {
     newRow = document.createElement("tr")
     newRow.className = carInfo.id
     swtTable.appendChild(newRow)
     createDataCells(carInfo, newRow, attributes)
 }
 
-createDataCells = (carInfo, newRow, attributes) => {
-    attributes.map((attribute) => {
+const createDataCells = (carInfo, newRow, attributes) => {
+    attributes.map(attribute => {
         newCell = document.createElement("td")
         newCell.innerHTML = carInfo[attribute]
         newRow.appendChild(newCell)
